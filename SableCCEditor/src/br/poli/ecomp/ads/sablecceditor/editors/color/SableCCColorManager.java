f0b82c2bc613d89ae946a20fc7b590218930418a
Gustavo Souto <gustavo.souto2@hotmail.com>
2017-01-09 23:45:08 -0200

    Added JavDoc

--- SableCCEditor/src/br/poli/ecomp/ads/sablecceditor/editors/color/SableCCColorManager.java
+++ SableCCEditor/src/br/poli/ecomp/ads/sablecceditor/editors/color/SableCCColorManager.java
@@ -8,6 +8,13 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
+/**
+ * Esta classe mapea cores RGB para o tipo de cor utilizado pelo Eclipse.
+ * 
+ * @author GUSTAVO, RENATO
+ *
+ */
+
 public class SableCCColorManager {
 
 	protected Map fColorTable = new HashMap(10);
@@ -17,6 +24,13 @@ public class SableCCColorManager {
 		while (e.hasNext())
 			 ((Color) e.next()).dispose();
 	}
+	
+	/**
+	 * 
+	 * @param rgb
+	 * @return
+	 */
+	
 	public Color getColor(RGB rgb) {
 		Color color = (Color) fColorTable.get(rgb);
 		if (color == null) {
